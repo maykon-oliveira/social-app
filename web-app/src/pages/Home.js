f4e2877261ecda481651685210fe45a441ccdb4b
Maykon Oliveira <maykonoliveira850@gmail.com>
2020-05-16 10:25:31 -0300

    Login page

--- web-app/src/pages/Home.js
+++ web-app/src/pages/Home.js
@@ -2,20 +2,27 @@ import React, { useEffect, useState } from "react";
 
 import Grid from "@material-ui/core/Grid";
 import Scream from "../components/Scream";
+import Axios from "axios";
 
 export const Home = () => {
   const [screams, setScreams] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("/screams")
-      .then((e) => e.json())
-      .then(setScreams);
+    Axios.get("/screams")
+      .then(({data}) => data)
+      .then(setScreams)
+      .then(() => setLoading(false));
   }, []);
 
   return (
     <Grid container spacing={4}>
       <Grid item sm={8} xs={12}>
-        {screams && screams.map((s) => <Scream scream={s} key={s.screamId} />)}
+        {!loading ? (
+          screams.map((s) => <Scream scream={s} key={s.screamId} />)
+        ) : (
+          <p>Loading...</p>
+        )}
       </Grid>
       <Grid item sm={4} xs={12}>
         <p>Profile</p>
--- web-app/src/pages/Login.js
+++ web-app/src/pages/Login.js
@@ -1,13 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import { Grid, TextField, Button, Typography } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Axios from "axios";
 
-const styles = {};
+const styles = {
+  form: {
+    textAlign: "center",
+  },
+  pageTitle: {
+    margin: "10px auto",
+  },
+  textField: {
+    margin: "10px auto",
+  },
+  wrapper: {
+    position: "relative",
+    margin: "10px auto",
+  },
+  buttonProgress: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12,
+  },
+  error: {
+    marginBottom: 10,
+  },
+};
 
-const Login = () => {
+const Login = ({ history, classes }) => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const onChange = ({ target: { name, value } }) =>
+    setForm({ ...form, [name]: value });
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    Axios.post("/login", form)
+      .then(({ jwt }) => jwt)
+      .then(() => {
+        history.push("/");
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+        setErrors(e.response.data);
+        setLoading(false);
+      });
+  };
+
   return (
-    <div>
-      <h1>Login page</h1>
-    </div>
+    <Grid container className={classes.form}>
+      <Grid item sm></Grid>
+      <Grid item sm>
+        <Typography variant="h2" className={classes.pageTitle}>
+          Login
+        </Typography>
+        <form noValidate onSubmit={onSubmit}>
+          <TextField
+            id="email"
+            name="email"
+            type="email"
+            label="Email"
+            helperText={errors.email}
+            error={!!errors.email}
+            className={classes.textField}
+            fullWidth
+            onChange={onChange}
+            value={form.email}
+          ></TextField>
+          <TextField
+            id="password"
+            name="password"
+            type="password"
+            label="Password"
+            helperText={errors.password}
+            error={!!errors.password}
+            className={classes.textField}
+            fullWidth
+            onChange={onChange}
+            value={form.password}
+          ></TextField>
+          {errors.general && (
+            <Typography className={classes.error} color="error">
+              {errors.general}
+            </Typography>
+          )}
+          <div className={classes.wrapper}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              disabled={loading}
+            >
+              Login
+            </Button>
+            {loading && (
+              <CircularProgress size={24} className={classes.buttonProgress} />
+            )}
+          </div>
+          <Typography>
+            <small>
+              Don't have an account? <Link to="/singup">signup</Link>
+            </small>
+          </Typography>
+        </form>
+      </Grid>
+      <Grid item sm></Grid>
+    </Grid>
   );
 };
 
